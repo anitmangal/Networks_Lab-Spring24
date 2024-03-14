219bd59ffbeedfc75b25465e6f0e129cf43d8c9a
krishkhimasia <khimasiakrish@gmail.com>
2024-03-15 00:31:11 +0530

    changed printf to perror

--- Assgn5/user2.c
+++ Assgn5/user2.c
@@ -10,29 +10,29 @@ int main(){
     socklen_t addr_size;
     
     if((sockfd = m_socket(AF_INET, SOCK_MTP, 0))<0){
-        printf("Error in socket creation\n");
+        perror("Error in socket creation\n");
         return 1;
     }
     
     char src_ip[16]="127.0.0.1";
     char dest_ip[16]="127.0.0.1";
-    uint16_t src_port=12346;
-    uint16_t dest_port=12345;
+    uint16_t src_port=3000;
+    uint16_t dest_port=4000;
 
     if(m_bind(src_ip, src_port, dest_ip, dest_port)<0){
-        printf("Error in binding\n");
+        perror("Error in binding\n");
         return 1;
     }
 
     int fd=open("new.txt", O_WRONLY | O_CREAT, 0666);
     if(fd<0){
-        printf("Error in opening/creating file\n");
+        perror("Error in opening/creating file\n");
         return 1;
     }
 
     while(m_recvfrom(sockfd, buffer, 1024, 0, (struct sockaddr*)&serverAddr, &addr_size)>0){
         if(write(fd, buffer, 1024)<0){
-            printf("Error in writing to file\n");
+            perror("Error in writing to file\n");
             return 1;
         }
     }
@@ -40,7 +40,7 @@ int main(){
     close(fd);
 
     if(m_close(sockfd)<0){
-        printf("Error in closing\n");
+        perror("Error in closing\n");
         return 1;
     }
 
